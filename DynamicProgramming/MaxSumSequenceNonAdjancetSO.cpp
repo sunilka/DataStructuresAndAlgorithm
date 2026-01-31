1e2c17e53180f37e22e1c0a51607af866c850f1a
sunilka <sunilkannur.98@gmail.com>
2026-01-31 14:52:37 +0530

    Space optimization of the max subsequence sum of non adjacent elements

---
+++ DynamicProgramming/MaxSumSequenceNonAdjancetSO.cpp
@@ -0,0 +1,26 @@
+// https://www.geeksforgeeks.org/problems/max-sum-without-adjacents2430/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    int findMaxSum(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+    
+        for (int i = 1; i < n; i++) {
+            int take = nums[i];
+            if (i > 1)
+                take += prev2;
+    
+            int notTake = 0 + prev;
+    
+            int curi = max(take, notTake);
+            prev2 = prev;
+            prev = curi;
+        }
+    
+        return prev;
+    }
+};
+
