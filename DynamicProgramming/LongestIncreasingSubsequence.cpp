5dc48064826bd7f0b52e8cc33b0f900ef83a7af3
sunilka <sunilkannur.98@gmail.com>
2026-01-31 10:47:14 +0530

    LIS

---
+++ DynamicProgramming/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        vector<int> ans;
+        ans.push_back(nums[0]);
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > ans.back()) {
+                ans.push_back(nums[i]);
+            } else {
+                auto it = lower_bound(ans.begin(), ans.end(), nums[i]);
+                *it = nums[i];
+            }
+        }
+        return ans.size();
+    }
+};
