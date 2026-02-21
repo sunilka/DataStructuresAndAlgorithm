948cd8a81dffafad28906651db4a96d535b8f352
sunilka <sunilkannur.98@gmail.com>
2026-02-21 21:20:22 +0530

    Bottom up DP

---
+++ Strings/MinInsertionStepToMakePalindromeBU.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = i + 1; j < n; j++) {
+                
+                if(s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1];
+                } 
+                else {
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+};
