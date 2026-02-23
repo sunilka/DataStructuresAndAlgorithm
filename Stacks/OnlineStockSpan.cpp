3fa8a70d9ddafba88812bbcd6fb8f45a75ab5108
sunilka <sunilkannur.98@gmail.com>
2026-02-23 23:10:14 +0530

    Online stock span

---
+++ Stacks/OnlineStockSpan.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/online-stock-span/
+#include<bits/stdc++.h>
+using namespace std;
+
+class StockSpanner {
+    stack<pair<int,int>> st;
+public:
+    StockSpanner() {}
+
+    int next(int price) {
+        int span = 1;
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
