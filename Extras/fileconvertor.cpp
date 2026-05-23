e307b49492ccdf967b8cc4c960dd7e1dce59645a
sunilka <sunilkannur.98@gmail.com>
2026-05-23 11:51:47 +0530

    add logic for md and cpp filees

--- Extras/fileconvertor.cpp
+++ Extras/fileconvertor.cpp
@@ -11,6 +11,8 @@ int main(){
         if(ele == '.' || ele == ' ') converted_file_name += '_';
         else converted_file_name += ele;
     }
-    cout<<"Converted file name is : \n"<<converted_file_name;
+    cout<<"Converted file name is : \n";
+    cout<<"Mark down file is : "<<converted_file_name<<".md"<<endl;
+    cout<<"CPP file name is : "<<converted_file_name<<".cpp";
     return 0;
 }
