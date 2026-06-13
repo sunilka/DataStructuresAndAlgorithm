48e89d445d24322d8c6777b2f38d07dac24e07bb
sunilka <sunilkannur.98@gmail.com>
2026-06-13 12:29:25 +0530

    technique 2 for find repeating and missing numbers

--- Phase2/D12-13June2026/TakeUForward/Find_the_repeating_and_missing_numbers.cpp
+++ Phase2/D12-13June2026/TakeUForward/Find_the_repeating_and_missing_numbers.cpp
@@ -36,14 +36,6 @@ vector<int> findTwoElement(vector<int>& arr) {
     return ans;
 }
 
-int main(){
-    vector<int> arr = {4, 3, 6, 2, 1, 1};
-    vector<int> ans = findTwoElement(arr);
-    cout<<"Printing the answer here "<<endl;
-    for(auto ele: ans) cout<<ele<<" ";
-    cout<<endl;
-}
-
 /*
 
 Complexity analysis:
@@ -52,3 +44,38 @@ Time complexity: O(N), where N is the number of elements present in the array.
 Space complexity: O(N), where N is the number of elements present in the array.
 
 */
+
+vector<int> findTwoElement(vector<int>& arr) {
+    long long n = arr.size();
+    long long sn = (n*(n+1))/2;
+    long long s2n = (n*(n+1)*(2*n+1))/6;
+    long long s = 0, s2 = 0;
+    for(int i=0; i<n; i++){
+        s+=(long long)arr[i];
+        s2+=(long long)arr[i]*(long long)arr[i];
+    }
+    long long val1 = s - sn;
+    long long val2 = s2 - s2n;
+    val2 = val2/val1;
+    long long x = (val1 + val2)/2;
+    long long y = x - val1;
+    return {(int)x, (int)y};
+}
+
+/*
+
+Complexity analysis:
+
+Time complexity: O(N), for the summation, where N is the number of elements present in the array.
+Space complexity: O(1), we do not use any extra space, expect some vairbales, hence the space complexity is constant. 
+
+*/
+
+int main(){
+    vector<int> arr = {4, 3, 6, 2, 1, 1};
+    vector<int> ans = findTwoElement(arr);
+    cout<<"Printing the answer here "<<endl;
+    for(auto ele: ans) cout<<ele<<" ";
+    cout<<endl;
+}
+
