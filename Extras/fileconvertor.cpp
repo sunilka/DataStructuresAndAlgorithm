0cca943e4f7cd2588d2785a573645c10bccd3fe3
sunilka <sunilkannur.98@gmail.com>
2026-06-17 09:08:02 +0530

    Seach in rotated array

--- Extras/fileconvertor.cpp
+++ Extras/fileconvertor.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Paste the file name here 
-string fname = "540. Single Element in a Sorted Array";
+string fname = "33. Search in Rotated Sorted Array";
 
 int main(){
     string converted_file_name  = "";
