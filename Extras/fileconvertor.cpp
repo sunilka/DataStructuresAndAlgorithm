0baffb295d234e4c53fd0161e034f4d59ec8d482
sunilka <sunilkannur.98@gmail.com>
2026-06-27 06:46:25 +0530

    Bipartite graph

--- Extras/fileconvertor.cpp
+++ Extras/fileconvertor.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // Paste the file name here 
-string fname = "785. Is Graph Bipartite?";
+string fname = "Directed Graph Cycle";
 
 int main(){
     string converted_file_name  = "";
