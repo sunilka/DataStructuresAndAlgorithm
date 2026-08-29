97b239a0fb80f58073c1555f64a53666f082e566
sunilka <sunilkannur.98@gmail.com>
2026-08-29 09:43:11 +0530

    Best time to buy and sell a stock

---
+++ Phase2/D59-29Aug2026/Revision/121__Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,47 @@
+/*
+121. Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size(), ans = 0;
+        int maxi = prices[n-1];
+        for(int i=n-2; i>=0; i--){
+            ans = max(ans, maxi - prices[i]);
+            maxi = max(maxi, prices[i]);
+        }
+        return ans;
+    }
+};
+
+/*
+Time complexity: O(N), where N is the number of elements present in the given prices array.
+Space complexity: O(1), we are not using any extra space, hence the space complexity is constant.
+*/
