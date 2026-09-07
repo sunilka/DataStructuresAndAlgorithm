dc3a0bab5c8c55ae1d564c0c1a61742b307d1dfd
sunilka <sunilkannur.98@gmail.com>
2026-09-07 09:28:57 +0530

    Detect cycle in a linked list

--- Extras/fileconvertor.cpp
+++ Extras/fileconvertor.cpp
@@ -3,7 +3,7 @@
 using namespace std;    
 
 // Paste the file name here 
-string fname = "876. Middle of the Linked List";
+string fname = "141. Linked List Cycle";
 int main(){
     string converted_file_name  = "";
     for(auto ele: fname){
