fe323bec13c5156c6b93da032bbe2393e59ac846
sunilka <sunilkannur.98@gmail.com>
2026-09-22 08:35:09 +0530

    Diameter of the binary tree

--- Extras/fileconvertor.cpp
+++ Extras/fileconvertor.cpp
@@ -3,7 +3,7 @@
 using namespace std;    
 
 // Paste the file name here 
-string fname = "110. Balanced Binary Tree";
+string fname = "543. Diameter of Binary Tree";
 int main(){
     string converted_file_name  = "";
     for(auto ele: fname){
